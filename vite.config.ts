2324d02047d0b7818fb9f3dd6e764d501ce0d2d9
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2024-03-01 13:26:13 +0300

    service: добавляет глобальные стили

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/styles/variables.scss"; @import "src/styles/mixins.scss";`,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
